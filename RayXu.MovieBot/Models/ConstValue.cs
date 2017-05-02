13a4d01043d8cabaf07f706e32cf23aa29b2f3ce
v-qihxu <v-qihxu@V-QIHXU-02>
2017-05-02 10:11:47 +0800

    add find_movie intent

--- RayXu.MovieBot/Models/ConstValue.cs
+++ RayXu.MovieBot/Models/ConstValue.cs
@@ -7,8 +7,16 @@ namespace RayXu.MovieBot.Models
 {
     public class ConstValue
     {
+        #region Intent
         public const string GreetingIntent = "greeting";
+        public const string NoneIntent = "None";
 
+        public const string FindMovieIntent = "find_movie";
+        #endregion
+
+        #region Default Reply
         public const string GreetingReply = "您好，我是小影，您的影视助手！";
+        public const string NoneReply = "对不起，我还不能回答这个问题！";
+        #endregion
     }
 }
